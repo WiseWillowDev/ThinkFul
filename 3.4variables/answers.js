cf886c4aa570c813c87b98a210b6e2f07e364411
WiseWillowDev <richardsperceptions@gmail.com>
2019-09-19 02:55:55 -0500

    Did the exersize

---
+++ 3.4variables/answers.js
@@ -0,0 +1,19 @@
+    // 1) If you want to create a variable called mentor and set its value equal to the name of your mentor, how would you do that?
+    let mentor = "Christophe Drayton";
+    
+    
+    // 2) How would you then log the value of mentor to the console?
+    console.log(mentor);
+
+    // 3) Describe the difference between let, var, and const?
+
+    // const = a constant unchangeable value, its like final in java
+    // let = is a variable that is changeable 
+    // var = is the same as let but older and it hated by the community apparently, 
+    //        there is a difference in scope of the variable with let, but i dont know that
+
+    // 4) Using var, let and const create 3 variables that will hold your name, state, and hair color?
+    const name = 'Richard';
+    let state = 'Texas';
+    var hairColor = "blue";
+
